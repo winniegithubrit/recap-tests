48ea789606181437fa9ba6beb0d10ebd8253c521
winniegithubrit <winnie.jomo@student.moringaschool.com>
2024-03-06 22:17:55 +0300

    Completed

---
+++ instances.py
@@ -0,0 +1,18 @@
+class Dog:
+  def sit(self):
+    print("The dog is sitting")
+    
+fido = Dog()
+fido.sit()
+
+
+class Person:
+  def talk(self):
+    print("Hello world")
+    
+  def walk(self):
+    print("The person is walking")
+  
+human = Person()
+human.talk()
+human.walk()
